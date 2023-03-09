80e8655925f317df729be9f612f4b7a5a933593b
sensen <2848294993@qq.com>
2023-03-09 21:23:06 +0800

    module、module.exports对象以及exports对象

---
+++ module.js
@@ -0,0 +1,9 @@
+const module1=require("./module1");
+console.log(module1);
+module1.sayHello();
+// {
+//     username: 'sensen',
+//     sex: 'male',
+//     fullname: 'senjie',
+//     sayHello: [Function (anonymous)]
+// }
---
+++ module1.js
@@ -0,0 +1,5 @@
+exports.name="senjie";
+module.exports.username="jiejie";
+module.exports.sayHello=function(){
+    console.log("hhhh");
+}
